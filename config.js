87d4f858a106c1fa650783cdfebe27c0e77cf939
KLM <122469699+KLMLEVRAI@users.noreply.github.com>
2024-10-15 08:08:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     app: {
         px: '&', // Le prefix exemple +help *help ta capter le prefix avant la commande...
-        owners: '1295624071912095794', // Ici tu te met owner (Que toi pas tes meilleurs collègue...)
-        funny: '1295624071912095794',  // Ici tu te met owner (Que toi pas tes meilleurs collègue...)
+        owners: '1100389168435179613', // Ici tu te met owner (Que toi pas tes meilleurs collègue...)
+        funny: '1100389168435179613',  // Ici tu te met owner (Que toi pas tes meilleurs collègue...)
         color: '#100238', // Couleur en HEX | https://htmlcolorcodes.com/fr/selecteur-de-couleur/
         footer: 'ice', // Quand tu fais +help par exemple y'a un texte en bas de l'embed...
         maxserver: '100', // Maximum de serveur ou ton bot peut etre dedans...
